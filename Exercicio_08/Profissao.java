f4f269c235786c86afb8f9acdf1a679a085b0f4c
Jhowjhow <137461345+JhowRossii@users.noreply.github.com>
2025-09-07 18:17:03 -0300

    Add files via upload

---
+++ Exercicio_08/Profissao.java
@@ -0,0 +1,9 @@
+class Profissao {
+    private String nome;
+    private double salario;
+    private String nivel;
+
+    public void usarFerramenta() {
+        System.out.println("Estou usando ferramentas para trabalhar melhor.");
+    }
+}
