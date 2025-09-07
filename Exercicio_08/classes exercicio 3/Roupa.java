5ae3976ba939417675579c3da84608367d41f939
mirandajhow <137461345+mirandajhow@users.noreply.github.com>
2025-09-07 18:46:35 -0300

    updates das pastas
    Arrumando as pastas do exercicio 8

--- Exercicio_08/classes exercicio 3/Roupa.java
+++
@@ -1,9 +0,0 @@
-class Roupa {
-    private String tipo;
-    private int tamanho;
-    private String estado;
-
-    public void vestirRoupa() {
-        System.out.println("Roupa vestida.");
-    }
-}
--- Exercicio_08/classes exercicio 4/Nacionalidade.java
+++
@@ -1,9 +0,0 @@
-class Nacionalidade {
-    private String pais;
-    private String origem;
-    private String identidade;
-
-    public void identificarCultura() {
-        System.out.println("Cultura identificada.");
-    }
-}
