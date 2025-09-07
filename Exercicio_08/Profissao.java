a2ce25ae8dd0be1ce17ba76db0fc972be9ce28c5
mirandajhow <137461345+mirandajhow@users.noreply.github.com>
2025-09-07 18:31:34 -0300

    Melhoria no layout
    Adicionada novas pastas para melhorar a organização.

--- Exercicio_08/Profissao.java
+++
@@ -1,9 +0,0 @@
-class Profissao {
-    private String nome;
-    private double salario;
-    private String nivel;
-
-    public void usarFerramenta() {
-        System.out.println("Estou usando ferramentas para trabalhar melhor.");
-    }
-}
---
+++ Exercicio_08/classes exercicio 5/Nacionalidade.java
@@ -0,0 +1,9 @@
+class Nacionalidade {
+    private String pais;
+    private String origem;
+    private String identidade;
+
+    public void identificarCultura() {
+        System.out.println("Cultura identificada.");
+    }
+}
